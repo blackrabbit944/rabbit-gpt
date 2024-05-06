cb6f90df90633b2a879a10e06d9785c4ed53c3bc
blackrabbit <rabbitblack944@gmail.com>
2024-05-06 11:58:50 +0900

    修改了UI

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -3,10 +3,9 @@ import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
 export default function Logo() {
     return (
         <div className="logo flex justify-start items-center">
-            <div className="text-green-500 mr-2">
+            <div className="text-logo mr-2">
                 <ChatBubbleLeftRightIcon className="h-8 w-8" />
             </div>
-            JianDa
         </div>
     );
 }
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -100,7 +100,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <DialogPrimitive.Description
         ref={ref}
-        className={cn("text-sm text-muted-foreground", className)}
+        className={cn("text-sm  theme-text-secondary", className)}
         {...props}
     />
 ));
